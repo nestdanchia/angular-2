0344fcb1c0f25e0244590c55ae364ce6042f0801
Nestor <chiariellonestordaniel@gmail.com>
2017-03-16 15:41:10 -0300

    intento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,21 @@
+import { MovimientosModule } from './movimientos/movimientos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+// desde cmd powershell
 import { AppComponent } from './app.component';
+
+// para crear un componente en una carpeta hay que dar su ruta ng g c movimientos/nuevo
+// si creo un componente con ng g c pepe este sera hijo de app y hermano de MovimientosComponent
+// el entorno incluira automaticamente lo anota como una declaracion mas 
+// dentro del modulo principal en app.module.ts en declarations a PepeComponent
+// asi sera conocido por cualquier componente del modulo principal puedo usar su vista en app.component.html
+// creamos luego desde la raiz de la aplicacion ng generate c movimientos/nuevo
+// asi NuevoComponent se declara automaticamente en movimientos.module.ts puedo utilizarlo en ese modulo
+// si quiero usarlo fuera de ese modulo debo exportarlo exports:[NuevoComponente]
+// como el modulo movimientos EN ESTE CASO se creo con ng g m MovimientosModule
+// debo importarlo al modulo app.module.ts import { MovimientosModule } from './movimientos/movimientos.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +24,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MovimientosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/movimientos/movimientos.module.ts
+++ src/app/movimientos/movimientos.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovimientoComponent } from './movimiento/movimiento.component';
 // generando a parir de un modulo 1 ng generate m movimientos 2. ng g c movimientos/movimiento
-//la herramienta actualiza el modulo declarando a MovimientoComponent
-//export:MovimientoComponent y luego en app.module.ts imports:MovimientosModule
-//webcomponent.org
+// la herramienta actualiza el modulo declarando a MovimientoComponent
+// export:MovimientoComponent y luego en app.module.ts imports:MovimientosModule
+// webcomponent.org
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [MovimientoComponent]
+  declarations: [MovimientoComponent],
+  exports: [MovimientoComponent] // debemos exportar al componente movimiento para que sea conocido desde la raiz app
 })
 export class MovimientosModule { }
