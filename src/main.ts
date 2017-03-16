279a10763bc233be515a98efe7291945ae47c42c
nestdanchia <ndchiariello@gmail.com>
2017-03-16 12:49:42 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+// generando a parir de un modulo 1 ng generate m movimientos 2. ng g c movimientos/movimiento
+//la herramienta actualiza el modulo declarando a MovimientoComponent
+//export:MovimientoComponent y luego en app.module.ts imports:MovimientosModule
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
